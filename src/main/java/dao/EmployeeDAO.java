166642a5c1af7c32af751a34ee49ce842a968c99
Qwaieare <gb-grand@mail.ru>
2023-05-19 03:32:58 +0300

    hw20230515_JDBC is done

---
+++ src/main/java/dao/EmployeeDAO.java
@@ -0,0 +1,19 @@
+package dao;
+
+import models.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    void create(Employee employee);
+
+    Employee readById(int id);
+
+    List<Employee> readAll();
+
+    void upDateEmployeeById(int id, Employee employee);
+
+    void deleteById(int id);
+
+
+}
